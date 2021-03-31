cca074c227d49caf6347b44cab940ef42d07e180
QingQiz <sofeeys@outlook.com>
2021-03-31 16:21:37 +0800

    feat: index redirect to swagger by default

---
+++ src/ApiHub.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ApiHub.Web.Pages
+{
+    public class Index : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            return Redirect("/swagger");
+        }
+    }
+}
